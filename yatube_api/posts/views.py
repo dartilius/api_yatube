222525b1a5c08628d808cb5e41abb834b3a0f788
borisovi <borisovi@krasrm.com>
2023-03-31 00:40:04 +0700

    Первое ревью 1

--- yatube_api/posts/views.py
+++ yatube_api/posts/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status
 from .models import Post, Group, Comment
 from .serializers import PostSerializer, GroupSerializer, CommentSerializer
 
